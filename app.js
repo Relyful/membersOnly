4e31f243625348a023788fbe5ddfab26c3617d47
Relyful <13381270+Relyful@users.noreply.github.com>
2025-05-12 22:26:44 +0200

    Initiate basic server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = 8080;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}. Rely.`));
