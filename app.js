0a3ace7d5a17978f5c6edf99cfdb88bf6ad3b404
Relyful <13381270+Relyful@users.noreply.github.com>
2025-05-13 20:47:43 +0200

    fix typo Public folder capitalization

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 //Set-up Public files
-const assetsPath = path.join(__dirname, "Public");
+const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
 app.use('/', indexRouter);
