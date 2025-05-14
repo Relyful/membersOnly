8aff8e31e2a8d9f63f8a729ecfb62c0314d9a2c4
Relyful <13381270+Relyful@users.noreply.github.com>
2025-05-14 21:55:01 +0200

    Setup passportjs local strategy and sessions

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const path = require('path');
+const pool = require("./db/pool");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const passport = require('passport');
+const LocalStrategy = require("passport-local").Strategy;
+
+
 const indexRouter = require('./routers/indexRouter');
 
 require('dotenv').config()
+const pgSession = require('connect-pg-simple')(session);
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+//setup for login sessions
+app.use(session({
+  store: new pgSession({
+    pool : pool                // Connection pool
+  }),
+  secret: process.env.SECRET,
+  resave: false,
+  cookie: { maxAge: 2 * 24 * 60 * 60 * 1000 }, // 2 days
+  saveUninitialized: true,
+}));
 
 //Set-up url request body parsing
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +34,48 @@ app.set("view engine", "ejs");
 //Set-up Public files
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+//Set-up for classic session
+app.use(passport.session());
+
+//Set-up passport strategy Try to remove try catch block for express v5
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => { //try to remove try catch block for new express
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 app.use('/', indexRouter);
 
