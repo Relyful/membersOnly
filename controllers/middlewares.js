68faeff9334e874ba5c7a770370f5d6cb6cd8fa3
Relyful <13381270+Relyful@users.noreply.github.com>
2025-05-19 21:03:46 +0200

    Add admin middleware

--- controllers/middlewares.js
+++ controllers/middlewares.js
@@ -4,3 +4,11 @@ exports.authMiddleware = (req, res, next) => {
   };
   res.status(403).send("You don't have permission to access this page");
 };
+
+
+exports.adminMiddleware = (req, res, next) => {
+  if (req.user.admin_status) {
+    return next();
+  };
+  res.status(403).send("You're not an admin! You can't be here!");
+};
